b5e038a25b7bf51c115cf65a33c54d3ba69bd909
Valera Sokolov <komproekt42@gmail.com>
2022-09-29 07:26:58 +0300

    While

--- index.php
+++ index.php
@@ -2,14 +2,18 @@
 
 echo 'Введите первое число ';
 fscanf(STDIN, "%d\n", $number1);
+while (is_int($number1) === false) {
+    fwrite(STDERR, 'Введите, пожалуйста, число ' . PHP_EOL);
+    fscanf(STDIN, "%d\n", $number1);
+}
 echo 'Введите второе число ';
 fscanf(STDIN, "%d\n", $number2);
-
-if ($number2 === 0) {
-   return fwrite(STDERR, 'На 0 делить нельзя' . PHP_EOL);
-}
-if (is_int($number1) && is_int($number2) ) {
-    fwrite(STDOUT, 'Результат ' . $number1 / $number2. PHP_EOL);
-} else {
+while (is_int($number2) === false) {
     fwrite(STDERR, 'Введите, пожалуйста, число ' . PHP_EOL);
+    fscanf(STDIN, "%d\n", $number2);
 }
+while ($number2 === 0) {
+    fwrite(STDERR, 'На 0 делить нельзя, введите другое число' . PHP_EOL);
+    fscanf(STDIN, "%d\n", $number2);
+}
+fwrite(STDOUT, 'Результат ' . $number1 / $number2 . PHP_EOL);
